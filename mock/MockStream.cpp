2f03c9b8fe7c480af97f055eeca4f3a031010b0b
Roout <following2future@gmail.com>
2021-03-28 19:54:43 +0300

    feature(MockStream): implement async read stream

--- mock/MockStream.cpp
+++ mock/MockStream.cpp
@@ -9,3 +9,11 @@ MockAsyncWriteStream::MockAsyncWriteStream(
     , m_buffer { buffer }
     , m_size { size }
 {}
+
+MockAsyncReadStream::MockAsyncReadStream(
+    boost::asio::io_context::executor_type& ex
+    , boost::asio::const_buffer buffer
+)
+    : m_executor { ex }
+    , m_buffer { buffer }
+{}
